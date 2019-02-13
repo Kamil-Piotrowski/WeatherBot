7edf24dd5a37bfa125521f2ecefdc848363bc17b
kamil.piotrowski <kamil.piotrowski@gatx.eu>
2019-02-13 16:59:23 +0100

    ...

--- WeatherBot/WeatherBotBot.cs
+++ WeatherBot/WeatherBotBot.cs
@@ -115,8 +115,16 @@ namespace WeatherBot
         private string getWeatherData(string searchText)
         {
             string locationKey = _weatherDataSource.GetLocationKey(searchText);
-            WeatherData res =  _weatherDataSource.GetWeatherData(locationKey);
-            return res.LocalTime + " - " + res.Description + " - " + res.TemperatureCelsius;
+            try
+            {
+                WeatherData res = _weatherDataSource.GetWeatherData(locationKey);
+                return res.LocalTime + " - " + res.Description + " - " + res.TemperatureCelsius;
+            }
+            catch
+            {
+                return "gdzie?";
+            }
+            
         }
     }
 }
